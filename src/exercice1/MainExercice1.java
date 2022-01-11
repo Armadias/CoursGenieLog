342d2656dbc9f4f7af593301cbc6e3cf90ed249c
Armadia <follietmartin@gmail.com>
2022-01-11 08:38:04 +0100

    Martin Folliet exo

--- src/exercice1/MainExercice1.java
+++ src/exercice1/MainExercice1.java
@@ -1,6 +1,7 @@
 package exercice1;
 
 import datamocklib.Person;
+import datamocklib.TxtHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,9 +10,18 @@ public class MainExercice1 {
 
     // Format du fichier : firstName,lastName,birthDate,gender,cityOfResidence,cityOfBirth
     // Pas de header
-    public List<Person> parse(List<String> data) {
+    public static List<Person> parse(List<String> data) {
         // A COMPLETER
-        return new ArrayList<Person>();
+        List<Person> personnes = new ArrayList<>();
+        for (String s : data)
+        {
+            String[] tokens = s.split(",");
+
+            Person p = new Person(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5]);
+
+            personnes.add(p);  
+        }
+    return personnes;
     }
 
     public static void main(String[] args) {
@@ -24,6 +34,19 @@ public class MainExercice1 {
          * Les données contiennent une trentaine de villes, Bonne chance !
         */
 
+        List<String> datafetched = TxtHelper.getDataFromTxt("persons.txt");
+
+        List<Person> personnes = MainExercice1.parse(datafetched);
+
+        System.out.println(personnes.get(0).toString());
+
+        Map<String, Integer> nbOfBirthPerCity = new Hashmap<String, Integer>();
+
+        for (Person p : personnes)
+        {
+            p.getCityOfBirth();
+            // récupérer les villes, chercher si la ville est dans le dico, ajouter la ville si non, mettre +1 à la ville si oui
+        }
 
     }
 }
